8cf8a2f5bae5d94f5203e7ad09cc91a4a48aa58c
snrogers <sebastian.n.rogers@gmail.com>
2018-07-12 16:09:48 -0400

    some heroku config

--- Procfile
+++ Procfile
@@ -2,3 +2,8 @@ dev-serve: npm run dev-serve
 dev-test-client: npm run dev-test-client
 dev-test-server: npm run dev-test-server
 dev-transpile: npm run dev-transpile
+
+build: npm run build
+db-create: sequelize db:create --config app/db/config/config.json
+db-migrate: sequelize db:migrate --migrations-path app/db/migrations/ --config app/db/config/config.json
+web: NODE_ENV=production npm start
---
+++ app/db/config/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  development: {
+    username: 'postgres',
+    password: null,
+    database: 'tree_challenge_development',
+    host: '127.0.0.1',
+    port: 5432,
+    dialect: 'postgres'
+  },
+  test: {
+    username: 'postgres',
+    password: null,
+    database: 'tree_challenge_test',
+    host: '127.0.0.1',
+    port: 5432,
+    dialect: 'postgres'
+  },
+  production: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: 'tree_challenge_production',
+    host: process.env.DATABASE_URL,
+    port: process.env.DATABASE_PORT,
+    dialect: 'postgres'
+  }
+};
