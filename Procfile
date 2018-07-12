92d6b938f60dbce8a08d225c8e846e10396230a0
snrogers <sebastian.n.rogers@gmail.com>
2018-07-12 17:52:12 -0400

    fix Procfile

--- Procfile
+++ Procfile
@@ -4,6 +4,5 @@ dev-test-server: npm run dev-test-server
 dev-transpile: npm run dev-transpile
 
 build: npm run build
-db-create: sequelize db:create --config app/db/config/config.json
-db-migrate: sequelize db:migrate --migrations-path app/db/migrations/ --config app/db/config/config.json
-web: NODE_ENV=production npm start
+db-create: npm run db-create
+web: NODE_ENV=production node app/build/server/server.js
--- app/db/config/config.js
+++ app/db/config/config.js
@@ -16,11 +16,12 @@ module.exports = {
     dialect: 'postgres'
   },
   production: {
-    username: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: 'tree_challenge_production',
-    host: process.env.DATABASE_URL,
-    port: process.env.DATABASE_PORT,
-    dialect: 'postgres'
+    use_env_variable: 'DATABASE_URL'
+    // username: process.env.PGUSER,
+    // password: process.env.PGPASSWORD,
+    // database: 'tree_challenge_production',
+    // host: process.env.DATABASE_URL,
+    // port: process.env.DATABASE_PORT,
+    // dialect: 'postgres'
   }
 };
